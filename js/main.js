b6b938f16ca96a5961725f501eb69e9056e130b1
Felipe Segura <fsegurar@unbosque.edu.co>
2021-09-09 18:51:20 -0500

    1 success

--- js/main.js
+++ js/main.js
@@ -36,6 +36,8 @@ fetch('https://api.thecatapi.com/v1/breeds')
             catBreed.appendChild(option);
         }); 
     });
+
+    
     
 document.getElementById("petspecies-input").onclick = function(){    
     let raza =document.getElementById("petspecies-input").value;
@@ -48,83 +50,97 @@ document.getElementById("petspecies-input").onclick = function(){
         document.getElementById("catraza-input").removeAttribute("disabled")
         document.getElementById("dograza-input").setAttribute("disabled",true)
     }
-}    
+};    
 
-document.getElementById("petsave-button").onclick = function savePet() {
-    rowId += 1;
-    let pet = {
-        dateinput : document.getElementById("date-input").value,
-        ownername: document.getElementById("owner-input").value,
-        petname: document.getElementById("petname-input").value,
-        microchipnum: document.getElementById("petmicrochip-input").value,
-        petage: document.getElementById("petage-input").value,
-        petspecie: document.getElementById("petspecies-input").value,
-        petsex: document.getElementById("petsex-input").value,
-        petsize: document.getElementById("petsize-input").value,
-        dangerousinput: document.getElementById("dangerous-input").value,
-        esterilizadoinput: document.getElementById("esterilizado-input").value,
-        neightborinput: document.getElementById("neightbor-input").value,
-        
-    };
+(function() {
+    var forms = document.getElementsByClassName('needs-validation');
+    var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+                console.log(form.checkValidity())
+                event.preventDefault();
+                event.stopPropagation();
+            }else if(form.checkValidity()===true){
+                rowId += 1;
+                let pet = {
+                    dateinput : document.getElementById("date-input").value,                        
+                    ownername: document.getElementById("owner-input").value,
+                    petname: document.getElementById("petname-input").value,
+                    microchipnum: document.getElementById("petmicrochip-input").value,
+                    petage: document.getElementById("petage-input").value,
+                    petspecie: document.getElementById("petspecies-input").value,
+                    petsex: document.getElementById("petsex-input").value,
+                    petsize: document.getElementById("petsize-input").value,
+                    dangerousinput: document.getElementById("dangerous-input").value,
+                    esterilizadoinput: document.getElementById("esterilizado-input").value,
+                    neightborinput: document.getElementById("neightbor-input").value,
+                        
+                };
 
-    let tr = document.createElement("tr")
-    tr.setAttribute("id", "row-" + rowId);
+                let tr = document.createElement("tr")
+                tr.setAttribute("id", "row-" + rowId);
 
-    let raza =document.getElementById("petspecies-input").value;
-    if(raza=="Perro"){
-        let tdimg = document.createElement("td");
-        let img = document.createElement("img");
-        let breed = document.getElementById("dograza-input").value;
-        fetch('https://dog.ceo/api/breed/' + breed + '/images/random')
-			.then(response => response.json())
-			.then(data => {
-                img.setAttribute("class","rounded")
-                img.setAttribute("alt","Cinque Terre")
-                img.setAttribute("width","200" )
-                img.setAttribute("height","156")
-				img.setAttribute("src",data.message);
-			}); 
-        tdimg.appendChild(img);
-	    tr.appendChild(tdimg); 
-    }else if(raza=="Gato"){
-        let tdimg = document.createElement("td");
-        let img = document.createElement("img");
-        let breedName = document.getElementById("catraza-input").value;
-		let breedId =catBreeds.find(breed => breedName==breed.name).id;
-        fetch('https://api.thecatapi.com/v1/images/search?breed_ids='+breedId)
-			.then(response => response.json())
-			.then(data => {
-				let catimage = data.map(cat=>cat.url);
-				
-				catimage.forEach(function(image)  {
-                    img.setAttribute("class","rounded")
-                    img.setAttribute("alt","Cinque Terre")
-                    img.setAttribute("width","200" )
-                    img.setAttribute("height","156")
-					img.setAttribute("src",image)
-				});
-            });    
-        tdimg.appendChild(img);
-	    tr.appendChild(tdimg); 
-    }
-    
-    Object.keys(pet).forEach((key) => {
-        let td = document.createElement("td");
-        td.innerHTML= pet[key];
-        tr.appendChild(td);
-    });
-    let tdActions = document.createElement("td");
+                let raza =document.getElementById("petspecies-input").value;
+                if(raza=="Perro"){
+                    let tdimg = document.createElement("td");
+                    let img = document.createElement("img");
+                    let breed = document.getElementById("dograza-input").value;
+                    fetch('https://dog.ceo/api/breed/' + breed + '/images/random')
+                    .then(response => response.json())
+                    .then(data => {
+                        img.setAttribute("class","rounded")
+                        img.setAttribute("alt","Cinque Terre")
+                        img.setAttribute("width","200" )
+                        img.setAttribute("height","156")
+                        img.setAttribute("src",data.message);
+                    }); 
+                    tdimg.appendChild(img);
+                    tr.appendChild(tdimg); 
+                }else if(raza=="Gato"){
+                    let tdimg = document.createElement("td");
+                    let img = document.createElement("img");
+                    let breedName = document.getElementById("catraza-input").value;
+                    let breedId =catBreeds.find(breed => breedName==breed.name).id;
+                    fetch('https://api.thecatapi.com/v1/images/search?breed_ids='+breedId)
+                    .then(response => response.json())
+                    .then(data => {
+                        let catimage = data.map(cat=>cat.url);
+                                
+                        catimage.forEach(function(image)  {
+                            img.setAttribute("class","rounded")
+                            img.setAttribute("alt","Cinque Terre")
+                            img.setAttribute("width","200" )
+                            img.setAttribute("height","156")
+                            img.setAttribute("src",image)
+                        });
+                    });    
+                    tdimg.appendChild(img);
+                    tr.appendChild(tdimg); 
+                }
+                    
+                Object.keys(pet).forEach((key) => {
+                    let td = document.createElement("td");
+                    td.innerHTML= pet[key];
+                    tr.appendChild(td);
+                });
+                let tdActions = document.createElement("td");
 
-	let input = document.createElement("input");
-    input.setAttribute("id", "delete-" + rowId);
-    input.setAttribute("type", "button");
-	input.value = "Eliminar";
-    input.onclick = function() {
-        let id = this.getAttribute("id");
-		id = +id.replace("delete-", "");
-		document.getElementById("row-" + id).remove();
-    };
-    tdActions.appendChild(input);
-	tr.appendChild(tdActions);
-	document.getElementById("body-table").appendChild(tr);
-};
+                let input = document.createElement("input");
+                input.setAttribute("id", "delete-" + rowId);
+                input.setAttribute("type", "button");
+                input.value = "Eliminar";
+                input.onclick = function() {
+                    let id = this.getAttribute("id");
+                    id = +id.replace("delete-", "");
+                    document.getElementById("row-" + id).remove();
+                };
+                tdActions.appendChild(input);
+                tr.appendChild(tdActions);
+                document.getElementById("body-table").appendChild(tr);
+                event.preventDefault();
+                event.stopPropagation();
+            };
+            form.classList.add('was-validated');
+        }, false);
+    });   
+})();
