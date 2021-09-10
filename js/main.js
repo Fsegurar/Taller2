2253b68efa62e691a54ecf60d94484a374221df5
Felipe Segura <fsegurar@unbosque.edu.co>
2021-09-10 11:19:38 -0500

    David mire

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 var rowId = 0;
 var catBreeds = [];
+var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+const dbName = "petDB";
 
 fetch("https://dog.ceo/api/breed/retriever/golden/images/random")
     .then(response => response.json())
@@ -37,7 +39,57 @@ fetch('https://api.thecatapi.com/v1/breeds')
         }); 
     });
 
+var request = indexedDB.open(dbName, 2);
+
+request.onerror = function(event) {
+    console.log("Database error");
+};
+request.onupgradeneeded = function(event) {
+    var db = event.target.result;
+    var objectStore = db.createObjectStore("pets", { keyPath: "id" });
+    objectStore.createIndex("petNameInput", "petNameInput", { unique: false });
+};
     
+var request = indexedDB.open(dbName, 2);
+request.onsuccess = function(event) {
+    var db = event.target.result;
+    var tx = db.transaction("pets");
+    var objectStore = tx.objectStore("pets");
+    objectStore.getAll().onsuccess = function(event) {
+        console.log(event.target.result);
+        rowId = event.target.result.length;
+        if(rowId>0){
+            let tr = document.createElement("tr")
+            tr.setAttribute("id", "row-" + rowId);
+            Object.keys(event.target.result).forEach(info => {
+                    console.log(event.target.result[info])
+            });
+            for(let i=0;i<rowId;i++){
+                tr.setAttribute("id", "row-" + (i+1));            
+                let tdimg = document.createElement("td");
+                let img = document.createElement("img");
+                let myLocalStorage = window.localStorage;
+                let imagen = myLocalStorage.getItem(i+1)
+                console.log(imagen)
+                img.setAttribute("class","rounded")
+                img.setAttribute("id","img-"+rowId)
+                img.setAttribute("alt","Cinque Terre")
+                img.setAttribute("width","200" )
+                img.setAttribute("height","156")
+                img.setAttribute("src",imagen)
+                tdimg.appendChild(img);
+                tr.appendChild(tdimg); 
+            }
+            Object.keys(event.target.result).map((info) => {
+                let td = document.createElement("td");
+                td.innerHTML= event.target.result[info];
+                tr.appendChild(td);
+            });
+            document.getElementById("body-table").appendChild(tr);
+        }
+    };
+    
+};    
     
 document.getElementById("petspecies-input").onclick = function(){    
     let raza =document.getElementById("petspecies-input").value;
@@ -76,10 +128,10 @@ document.getElementById("petspecies-input").onclick = function(){
                     neightborinput: document.getElementById("neightbor-input").value,
                         
                 };
-
+               
                 let tr = document.createElement("tr")
                 tr.setAttribute("id", "row-" + rowId);
-
+                let imagenperro = "";
                 let raza =document.getElementById("petspecies-input").value;
                 if(raza=="Perro"){
                     let tdimg = document.createElement("td");
@@ -90,9 +142,12 @@ document.getElementById("petspecies-input").onclick = function(){
                     .then(data => {
                         img.setAttribute("class","rounded")
                         img.setAttribute("alt","Cinque Terre")
+                        img.setAttribute("id","img-"+rowId)
                         img.setAttribute("width","200" )
                         img.setAttribute("height","156")
                         img.setAttribute("src",data.message);
+                        let myLocalStorage = window.localStorage;
+            	        myLocalStorage.setItem(rowId, data.message);
                     }); 
                     tdimg.appendChild(img);
                     tr.appendChild(tdimg); 
@@ -108,10 +163,13 @@ document.getElementById("petspecies-input").onclick = function(){
                                 
                         catimage.forEach(function(image)  {
                             img.setAttribute("class","rounded")
+                            img.setAttribute("id","img-"+rowId)
                             img.setAttribute("alt","Cinque Terre")
                             img.setAttribute("width","200" )
                             img.setAttribute("height","156")
                             img.setAttribute("src",image)
+                            let myLocalStorage = window.localStorage;
+            	            myLocalStorage.setItem(rowId, image);
                         });
                     });    
                     tdimg.appendChild(img);
@@ -134,13 +192,27 @@ document.getElementById("petspecies-input").onclick = function(){
                     id = +id.replace("delete-", "");
                     document.getElementById("row-" + id).remove();
                 };
+            
                 tdActions.appendChild(input);
                 tr.appendChild(tdActions);
                 document.getElementById("body-table").appendChild(tr);
                 event.preventDefault();
                 event.stopPropagation();
+                            
+                
+
+                var request = indexedDB.open(dbName, 2);
+                request.onsuccess = function(event) {
+                   var db = event.target.result;
+                   var customerObjectStore = db.transaction("pets", "readwrite").objectStore("pets");
+                   pet["id"] = rowId;
+                   
+                   customerObjectStore.add(pet);
+               };
+               console.log(document.getElementById("row-"+rowId))
             };
             form.classList.add('was-validated');
         }, false);
     });   
 })();
+
